4e44264b11016f31c7fc0289aa0176c28aa2b0f8
Ritika Srivastava <rsritika12@gmail.com>
2017-06-13 15:51:10 -0500

    initial commit

---
+++ src/main/java/com/operations/arithmatic/ArithmaticApplication.java
@@ -0,0 +1,12 @@
+package com.operations.arithmatic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArithmaticApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArithmaticApplication.class, args);
+	}
+}
---
+++ src/test/java/com/operations/arithmatic/ArithmaticApplicationTests.java
@@ -0,0 +1,16 @@
+package com.operations.arithmatic;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ArithmaticApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
